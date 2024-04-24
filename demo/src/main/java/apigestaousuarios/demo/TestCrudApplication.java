ce739f6a12fa22d123db9885ee63a72a400974f3
USERSAD\ttissoni <tatiane.tissoniantunes@everis.nttdata.com>
2024-04-24 09:42:32 -0300

    Adicionando estrutura spring boot

---
+++ demo/src/main/java/apigestaousuarios/demo/TestCrudApplication.java
@@ -0,0 +1,13 @@
+package apigestaousuarios.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCrudApplication.class, args);
+	}
+
+}
